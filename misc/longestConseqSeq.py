26b4cc35cb395ad756822807698bd0fe61d59b80
Victor Fei <vjhfei@gmail.com>
2017-05-24 18:58:39 -0700

    more

--- misc/longestConseqSeq.py
+++ misc/longestConseqSeq.py
@@ -10,7 +10,7 @@ The longest consecutive elements sequence is [1, 2, 3, 4]. Return its length: 4.
 Your algorithm should run in O(n) complexity.
 
 #128
-REDO: unique algo never seen before
+REDO: unique algo never seen before, know both algos
 """
 def findLongest(nums):
     dic = {}
@@ -28,9 +28,25 @@ def findLongest(nums):
 
     return res
 
+def findLongestSet(nums):
+    """
+    more concise and faster solution
+    """
+    s = set(nums)
+    res = 0
+    for x in s:
+        if x-1 not in s:
+            y = x+1
+            while y in s:
+                y += 1
+            res = max(res, y-x)
+
+    return res
+
 def test1():
     nums = [100, 4, 200, 1, 3, 2]
     print(findLongest(nums))
+    print(findLongestSet(nums))
 
 if __name__ == '__main__':
     test1()
